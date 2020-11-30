a402f7ec9bd7d181e0593023407c60a38a0044f3
ChauChorHim <66714382+ChauChorHim@users.noreply.github.com>
2020-11-30 15:41:54 +0100

    Update goto_markers.py

--- goto_markers.py
+++ goto_markers.py
@@ -2,63 +2,48 @@
 
 import numpy as np
 import rospy
-import actionlib
-import nav_msgs.msg
-import re
-import tf
+import re # regular expression
 import math
 import copy
-
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from nav_msgs.msg import Odometry
 from std_msgs.msg import String, Int8
 from geometry_msgs.msg import PoseStamped, Twist
-from scipy.optimize import root, fsolve
+
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+import tf
 from tf.listener import TransformListener
 from tf.transformations import quaternion_from_euler
-"""
-waypoints = [
-    [(1.13, -1.6, 0.0), (0.0, 0.0, -0.16547, -0.986213798314)],
-    [(0.81, 0.32, 0.0), (0.0, 0.0, -0.99, 0.0333)]
-]
-"""
 
-global qr_code_info
+from nav_msgs.msg import Odometry
+
+
+global qr_code_info # secret frame coordinates and qr code tag read from the topic '/visp_auto_tracker/code_message'; ["", "", "", "", ""]
 qr_code_info = []
 
-global objects_secret_position
+global objects_secret_position # secret frame coordinates of all qr code markers; [[, , 0, 1], [, , 0, 1]]
 objects_secret_position = []
-global object_secret_position
+global object_secret_position # secret frame coordinates of current qr code marker read from the topic '/visp_auto_tracker/object_position'; [, , 0, 1]
 object_secret_position = []
 
-global next_objects_secret_position
-next_objects_secret_position = []
-
-global robot_odom_position
-robot_odom_position = []
-
-global objects_odom_position
+global objects_odom_position # odom frame coordinates of all qr code markers; [[, , 0, 1], [, , 0, 1]]
 objects_odom_position = []
-global object_odom_position
+global object_odom_position # odom frame coordinates of current qr code marker; [, , 0, 1]
 object_odom_position = []
 
-global next_objects_odom_position
-next_objects_odom_position = []
-global next_object_secret_position
+global next_object_secret_position # secret frame coordinates of next qr code marker read from the topic '/visp_auto_tracker/object_position'; [, , 0, 1]
 next_object_secret_position = []
 
-global SMaRt_element
+global SMaRt_element # last character read from the topic '/visp_auto_tracker/code_message'; ''
 SMaRt_element = ''
-global SMaRt_messages
+global SMaRt_messages # all SMart_element; '{'1': 'S', '2': 'M', '3': 'a', '4': 'R', '5': 't'}'
 SMaRt_messages = {}
 
 
-global tags
+global tags # all tag; ['', '', '', '', '']
 tags = ['0']
-global tag
+global tag # fifth number read from the topic '/visp_auto_tracker/code_message'; ''
 tag = '0'
-global code_status
-code_status = 0
 
 def goal_pose(pose):
     if pose == []:
@@ -75,35 +60,19 @@ def goal_pose(pose):
     goal_pose.target_pose.pose.orientation.w = pose[1][3]
     return goal_pose
 
-def status_callback(message):
-    global code_status
-    code_status=message.data
-
-def odom_callback(message):
-    global robot_odom_position
-    robot_odom_position = nav_msgs.msg.Odometry()
-    robot_odom_position = message
-    br = tf.TransformBroadcaster()
-    br.sendTransform((robot_odom_position.pose.pose.position.x, 
-        robot_odom_position.pose.pose.position.y, 0), 
-        tf.transformations.quaternion_from_euler(0, 0, 0), 
-        rospy.Time.now(), 
-        "robot_map_position", 
-        "odom")
-
 def trans_secret_odom(objects_odom_position, objects_secret_position):
     #print(objects_odom_position)
     #print(objects_secret_position)
-    """
-    A = np.array([[float(objects_secret_position[0][0]), -float(objects_secret_position[0][1]), 1, 0],
-            [float(objects_secret_position[0][1]), float(objects_secret_position[0][0]), 0, 1],
-            [float(objects_secret_position[1][0]), -float(objects_secret_position[1][1]), 1, 0],
-            [float(objects_secret_position[1][1]), float(objects_secret_position[1][0]), 0, 1]])
+    
+    # A = np.array([[float(objects_secret_position[0][0]), -float(objects_secret_position[0][1]), 1, 0],
+    #         [float(objects_secret_position[0][1]), float(objects_secret_position[0][0]), 0, 1],
+    #         [float(objects_secret_position[1][0]), -float(objects_secret_position[1][1]), 1, 0],
+    #         [float(objects_secret_position[1][1]), float(objects_secret_position[1][0]), 0, 1]])
 
-    b = np.array([[objects_odom_position[0][0], objects_odom_position[0][1], objects_odom_position[1][0], objects_odom_position[1][1]]]).T
+    # b = np.array([[objects_odom_position[0][0], objects_odom_position[0][1], objects_odom_position[1][0], objects_odom_position[1][1]]]).T
 
-    results = np.linalg.solve(A, b)
-    """
+    # results = np.linalg.solve(A, b)
+    
     sub_x_world_12 = float(objects_odom_position[0][0]) - float(objects_odom_position[1][0])
     sub_y_world_12 = float(objects_odom_position[0][1]) - float(objects_odom_position[1][1])
     sub_x_secret_12 = float(objects_secret_position[0][0]) - float(objects_secret_position[1][0])
@@ -140,7 +109,12 @@ def eulerAnglesToRotationMatrix(r, p, y):
 def object_position_callback(message):
     global object_camera_position
     object_camera_position = [message.pose.position.x, message.pose.position.y, message.pose.position.z, 1]
-            
+    br.sendTransform((message.pose.position.x, message.pose.position.y, 0),
+                     (message.pose.orientation.x, message.pose.orientation.y, message.pose.orientation.z, message.pose.orientation.w),
+                     rospy.Time.now(),
+                     "qr_code",
+                     "camera_optical_link")
+             
 def code_message_callback(message):
     global qr_code_info
     global object_secret_position
@@ -154,22 +128,35 @@ def code_message_callback(message):
         tag = qr_code_info[4]
         SMaRt_element = str(message)[-2]
 
+
+def goto_observe_position(observe_position):
+    goal_position = goal_pose(observe_position)
+    client.send_goal(goal_position)
+    client.wait_for_result()
+    rospy.sleep(2)
+    print "arrived!" 
+
 if __name__ == '__main__':
     rospy.init_node('patrol')
+    rate = rospy.Rate(10)
+
     client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
     client.wait_for_server()
-    rate = rospy.Rate(10)
-    #rospy.Subscriber("/odom", Odometry, odom_callback)
-    Cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+    br = tf.TransformBroadcaster()
     listener = TransformListener()
-    rospy.Subscriber("visp_auto_tracker/status", Int8, status_callback, queue_size=10)
+
+    Cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     rospy.Subscriber("visp_auto_tracker/code_message", String, code_message_callback, queue_size=10)
     rospy.Subscriber("visp_auto_tracker/object_position", PoseStamped, object_position_callback, queue_size=10)
 
-    goal_position = goal_pose([[-4, 0, 0], [0, 0, 0, 0]])
-    client.send_goal(goal_position)
-    client.wait_for_result()
+    # go to the first observation position
+    first_observe_pose = [[-4.5, 0, 0], [0, 0, 0, 1]]
+    print "Go to the first observation position", "(", first_observe_pose[0][0], ", ", first_observe_pose[0][1], ")"
+    goto_observe_position(first_observe_pose)
 
+    print "=================================================="
+    print "Searching the first two qr code markers..."
     while(np.size(tags)<3):
         object_odom_position_solved = False
         twist_1 = Twist()
@@ -177,22 +164,22 @@ if __name__ == '__main__':
         Cmd_vel.publish(twist_1)
         while not object_odom_position_solved:
             if tag not in tags:
-                print("tag:",tag)
+                print("Find tag:",tag)
                 tags.append(tag)
                 SMaRt_messages.update({int(tag): SMaRt_element})
                 twist_2 = Twist()
                 Cmd_vel.publish(twist_2)
-                rospy.sleep(3)
+                rospy.sleep(1)
                 try:
                     # obtatin object_odom_position
-                    (trans, rot) = listener.lookupTransform('/odom', '/camera_optical_link', rospy.Time(0))
-                    (r_cam, p_cam, y_cam) = tf.transformations.euler_from_quaternion([rot[0], rot[1], rot[2], rot[3]])
-                    trans_rotation_cam_odom = np.insert(eulerAnglesToRotationMatrix(r_cam, p_cam, y_cam), 3, [0, 0, 0], 0)
-                    trans_translation_cam_odom = [trans[0], trans[1], trans[2], 1]
-                    trans_cam_odom = np.insert(trans_rotation_cam_odom, 3, trans_translation_cam_odom, 1)
+                    (trans, rot) = listener.lookupTransform('/odom', '/qr_code', rospy.Time(0))
+                    #(r_cam, p_cam, y_cam) = tf.transformations.euler_from_quaternion([rot[0], rot[1], rot[2], rot[3]])
+                    #trans_rotation_cam_odom = np.insert(eulerAnglesToRotationMatrix(r_cam, p_cam, y_cam), 3, [0, 0, 0], 0)
+                    #trans_translation_cam_odom = [trans[0], trans[1], trans[2], 1]
+                    #trans_cam_odom = np.insert(trans_rotation_cam_odom, 3, trans_translation_cam_odom, 1)
 
-                    object_odom_position = np.dot(trans_cam_odom, object_camera_position)
-                    objects_odom_position.append(object_odom_position)
+                    #object_odom_position = np.dot(trans_cam_odom, object_camera_position)
+                    objects_odom_position.append([trans[0], trans[1], 0, 1])
 
                     objects_secret_position.append(object_secret_position)
                 except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
@@ -203,22 +190,23 @@ if __name__ == '__main__':
             # print("objects_odom_position: ", objects_odom_position)
             # print("objects_secret_position: ", objects_secret_position)
             trans_secret_odom = trans_secret_odom(objects_odom_position, objects_secret_position)
-            print("The transform from secret frame to odom frame is: ")
-            print(trans_secret_odom)
+            print "=================================================="
+            print "The transform from secret frame to odom frame is: "
+            print trans_secret_odom
+            print "=================================================="
 
     while object_odom_position_solved:
-        print(SMaRt_messages)
-        if np.size(tags) == 6:
+        if len(SMaRt_messages) == 5:
             break
-        print("next_object_secret_position", next_object_secret_position)
         goal_tag = tag
-        print("tag: ", tag)
-        next_robot_secret_position = [float(next_object_secret_position[0])*0.75, float(next_object_secret_position[1])*0.75]
+        print "=================================================="
+        print "Current tag: ", tag 
+        next_robot_secret_position = [float(next_object_secret_position[0])*0.65, float(next_object_secret_position[1])*0.65]
         next_robot_odom_position = np.dot(trans_secret_odom, np.array(np.insert(next_robot_secret_position, 2, [0, 1], 0), dtype='float64'))
         next_robot_odom_position = [next_robot_odom_position, [0,0,0,1]]
 
-        
-        print("next_robot_odom_position", next_robot_odom_position)
+        print "next_robot_odom_position: "
+        print next_robot_odom_position
         goal_position = goal_pose(next_robot_odom_position)
         client.send_goal(goal_position)
         client.wait_for_result()
@@ -237,22 +225,5 @@ if __name__ == '__main__':
             continue
         
     SMaRt_messages = sorted(SMaRt_messages.items(), key=lambda x:x[0])
-    print(SMaRt_messages)
-"""
-        next_point = [(robot_map_position.pose.pose.position.x, robot_map_position.pose.pose.position.y, 0), (0, 0, 0, 1)]
-        goal_rotation = goal_pose(next_point)
-        client.send_goal(goal_rotation)
-        client.wait_for_result()
-"""
-
-"""
-        goal = goal_pose(next_point)
-        client.send_goal(goal)
-        client.wait_for_result()
-        next_point = [(next_point[0][0], next_point[0][1], next_point[0][2]), (0, 0, 0, 1)]
-        goal_rotation = goal_pose(next_point)
-        client.send_goal(goal_rotation)
-        client.wait_for_result()
-        flag1=True;
-        rospy.sleep(3)
-"""
+    print "=================================================="
+    print SMaRt_messages
